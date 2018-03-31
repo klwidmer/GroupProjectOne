57322d984e2c04ac8d3a251aa83a01a713f6021f
Becky Afzali <beckyafzali@gmail.com>
2018-03-31 13:48:23 -0500

    quote api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,44 @@
+$('document').ready(function() {
 // open weather app API info
+  const zipCode = '55104';
+  const queryURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=01189fd67504d20ce5fd075bdd8800ea`;
 
-const zipCode = '55104';
-const queryURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=01189fd67504d20ce5fd075bdd8800ea`;
+  $.ajax({
+    url: queryURL,
+    method: 'GET',
+  }).then(function(response) {
+    console.log(response);
+  });
 
-$.ajax({
-  url: queryURL,
-  method: 'GET',
-}).then(function(response) {
-  console.log(response);
+  // wunderground API
+
+  const zipCode2 = '55104';
+  const weatherKey = '97be962b96e69fba';
+  const weatherUrl = `http://api.wunderground.com/api/${weatherKey}/conditions/q/${zipCode2}.json`;
+
+  $.ajax({
+    url: weatherUrl,
+    dataType: 'jsonp',
+    method: 'GET',
+  }).then(function(response) {
+    console.log(response);
+  });
+
+  $.ajax({
+    url: 'https://api.forismatic.com/api/1.0/',
+    jsonp: 'jsonp',
+    dataType: 'jsonp',
+    data: {
+      method: 'getQuote',
+      lang: 'en',
+      format: 'jsonp',
+    },
+  }).then(function(response) {
+    console.log(response);
+    $('#quote').html(response.quoteText);
+    const authorPtag = $('<p id="author">');
+    $('#quote').append(authorPtag);
+    $('#author').html(response.quoteAuthor);
+  });
 });
 
-// wunderground API
-
-const zipCode2 = '55104';
-const weatherKey = '97be962b96e69fba';
-const weatherUrl = `http://api.wunderground.com/api/${weatherKey}/conditions/q/${zipCode2}.json`;
-
-$.ajax({
-  url: weatherUrl,
-  dataType: 'jsonp',
-  method: 'GET',
-}).then(function(response) {
-  console.log(response);
-});
-
-// Random quote API (need to replace b/c of 10 call/hour limit)
-const quoteURL = 'http://quotes.rest/qod.json';
-
-$.ajax({
-  url: quoteURL,
-  method: 'GET',
-  cache: false,
-}).then(function(response) {
-  console.log(response);
-});
